ecac6f3cae6053b4a698187452496e31008b3558
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-11-20 16:41:38 +0200

    FilteredList

--- DynamicallyFiltering@FetchRequestWithSwiftUICoreDataSwiftUI/FilteredList.swift
+++ DynamicallyFiltering@FetchRequestWithSwiftUICoreDataSwiftUI/FilteredList.swift
@@ -9,18 +9,20 @@
 import SwiftUI
 import CoreData
 
-struct FilteredList: View {
-    var fetchRequest: FetchRequest<Singer>
-    var singers: FetchedResults<Singer> { fetchRequest.wrappedValue }
+struct FilteredList<T: NSManagedObject, Content: View>: View {
+    var fetchRequest: FetchRequest<T>
+    var singers: FetchedResults<T> { fetchRequest.wrappedValue }
+    let content: (T) -> Content
     
     var body: some View {
         List(fetchRequest.wrappedValue, id: \.self) { singer in
-            Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
+            self.content(singer)
         }
     }
     
-    init(filter: String) {
-        fetchRequest = FetchRequest<Singer>(entity: Singer.entity(), sortDescriptors: [], predicate: NSPredicate(format: "lastName BEGINSWITH %@", filter))
+    init(filterKey: String, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
+        fetchRequest = FetchRequest<T>(entity: T.entity(), sortDescriptors: [], predicate: NSPredicate(format: "%K BEGINSWITH %@", filterKey, filterValue))
+        self.content = content
     }
 }
 
